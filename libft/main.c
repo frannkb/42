665138306788737f741d2a5f189f6f727d2f225f
Frank Bruno <frannk.bruno@gmail.com>
2023-10-26 12:57:03 -0300

    add ft_isdigit ft_isalnum

---
+++ libft/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "functions.h"
+
+
+int main()
+{
+    // FT_ISALPHA
+        // char letter = '3';
+        // int num = ft_isalpha(letter);
+        // printf("%d", num);
+    // FT_ISDIGIT
+        // char    num = 'O';
+        // int result1 = ft_isdigit(num);
+        // int result2 = isdigit(num);
+        // printf("My function: %d \nReal function: %d", result1, result2);
+    // FT_ISALNUM
+        // char lettlernum = '1';
+        // int result1 = ft_isalnum(lettlernum);
+        // int result2 = isalnum(lettlernum);
+        // printf("My function: %d\nReal function: %d", result1, result2);
+
+}
