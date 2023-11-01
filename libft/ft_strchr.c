f9e70d7abd43af39de8a85c3aa00cca055789560
Frank Bruno Santos Teixeira <fbruno-s@c1r6p1.42sp.org.br>
2023-10-31 22:20:02 -0300

    add ft_strchr.c

---
+++ libft/ft_strchr.c
@@ -0,0 +1,12 @@
+char *ft_strchr(const char *s, int c)
+{
+    int i;
+
+    i = 0;
+    while (s[i] != '\0' && s[i] != c)
+        i++;
+    if (s[i] == c)
+        return ((char *)&s[i]);
+    else
+        return (NULL);
+}
